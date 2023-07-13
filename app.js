88aa5ef7a7325c19394b20adefb418587b09c55f
Eyub197 <eyubmehmed122@gmail.com>
2023-07-13 11:14:01 +0300

    on hover the grid is red

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const containerDiv = document.querySelector(".container")
+let gridItem;
 
 for (let i = 0; i < 16; i++ ){
-    for(let j = 0; i< 16, i++){
-        let gridItem = document.createElement("div")
+    for(let j = 0; j< 16; j++){
+        gridItem = document.createElement("div")
+        gridItem.classList = "grid-item"
         containerDiv.appendChild(gridItem)
+
     }
     containerDiv.appendChild(document.createElement("br"))
 }
+
+const gridItems = document.querySelectorAll(".grid-item") 
+
+gridItems.forEach(item => {
+    item.addEventListener("mouseover", ()=> { item.style.backgroundColor = "red"})
+})
