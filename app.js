757a311ee5a5dd3d28b323a0a6a23e0ce9f1a073
Eyub197 <eyubmehmed122@gmail.com>
2023-07-13 11:58:57 +0300

    made a romoveAll functions to remove the old grid

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 const containerDiv = document.querySelector(".container")
-let gridItem;
+let btnChangeGrid = document.querySelector("button")
+let gridItem = 0;
 
-for (let i = 0; i < 16; i++ ){
-    for(let j = 0; j< 16; j++){
+let  createDivs =(count = 16) => {
+for (let i = 0; i < count; i++ ){
+    for(let j = 0;  j < count; j++){
         gridItem = document.createElement("div")
         gridItem.classList = "grid-item"
         containerDiv.appendChild(gridItem)
-
     }
     containerDiv.appendChild(document.createElement("br"))
+ }
+}
+
+createDivs()
+
+let removeAll = () => {
+    while(containerDiv.firstChild){
+        containerDiv.firstChild.remove
+    }
 }
 
 const gridItems = document.querySelectorAll(".grid-item") 
-
 gridItems.forEach(item => {
     item.addEventListener("mouseover", ()=> { item.style.backgroundColor = "red"})
 })
+
+
