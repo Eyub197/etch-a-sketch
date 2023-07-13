5e22e94430514475b6c67c02b8ce7044ea0d9fab
Eyub197 <eyubmehmed122@gmail.com>
2023-07-13 10:06:20 +0300

    creating the 16X16 grid

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 const containerDiv = document.querySelector(".container")
+
+for (let i = 0; i < 16; i++ ){
+    for(let j = 0; i< 16, i++){
+        let gridItem = document.createElement("div")
+        containerDiv.appendChild(gridItem)
+    }
+    containerDiv.appendChild(document.createElement("br"))
+}
