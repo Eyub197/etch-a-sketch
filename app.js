db0e8b8db7a879fed5fc3c4a2d816b872cd069d2
Eyub197 <eyubmehmed122@gmail.com>
2023-07-15 15:00:11 +0300

    The final functionality upadtes propably later if I get an idea

--- app.js
+++ app.js
@@ -6,16 +6,15 @@ const btnWowoMode = document.querySelector(".wowo-Mode")
 const btnGumichka = document.querySelector(".gumichka")
 const colorPicker = document.querySelector("input")
 const btnColorPicker = document.querySelector(".color-picker")
+const btnBigSize = document.querySelector(".big-size")
+const btnMediumSize = document.querySelector(".medium-size")
+const btnSmallSize = document.querySelector(".small-size")
 let gridItems =[]
-
-
 
 
 function createDivs(size = 16) {
   removeAll();
-
-  const squareSize = 400 / size;
-
+  const squareSize = 600 / size;
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
       const gridItem = document.createElement("div");
@@ -39,11 +38,8 @@ function removeAll() {
 }
 
 btnChangeGrid.addEventListener('click', () => {
-  let inputField = document.createElement("input")
-  inputField.type = "text"
-  
   let size = parseInt(prompt("Enter a grid size you like (max 64)"));
-  if (!isNaN(size) && size > 0 && size <= 64) {
+  if (!isNaN(size) && size > 0 && size <= 100) {
     createDivs(size);
   } else {
     alert("Error! Enter a valid number between 1 and 64.");
@@ -53,7 +49,6 @@ btnChangeGrid.addEventListener('click', () => {
 btnClearColor.addEventListener('click', ()=> { 
   gridItems.forEach((item) => item.style.backgroundColor = "#808080" ) } )
 
-btnRandomColor
 
 let generateRandomColor = () => {
   let randomColor = Math.floor(Math.random() * 16777216)
@@ -62,25 +57,27 @@ let generateRandomColor = () => {
 }
 
 btnRandomColor.addEventListener('click', ()=> {
-  const randomColor = generateRandomColor()
-  gridItems.forEach((item => item.addEventListener('mouseover', ()=>{
-  item.style.backgroundColor = randomColor
- })))
+    const randomColor = generateRandomColor()
+    gridItems.forEach((gridItem )=> gridItem.addEventListener('mouseover', ()=>{
+    gridItem.style.backgroundColor = randomColor
+ }))
 })
 
 //!Extara credits
 
 let generateRandomRgbColor = () => {
+
   let r = Math.floor(Math.random() * 256)
   let g = Math.floor(Math.random() * 256)
   let b = Math.floor(Math.random() * 256)
   let color = `rgb(${r}, ${g}, ${b})`
   return color
+
 }
 
-let darkenColor = (item) => {
+let darkenColor = (gridItem) => {
   let color = item.style.backgroundColor;
-  if (!item) return;
+  if (!gridItem) return;
 
   const rgbValues = color.match(/\d+/g);
   let r = parseInt(rgbValues[0]);
@@ -95,10 +92,10 @@ let darkenColor = (item) => {
 }
 
 btnWowoMode.addEventListener('click', ()=>{
-  gridItems.forEach((item) => {
+  gridItems.forEach((gridItem) => {
     item.addEventListener('mouseover', ()=>{
       item.style.backgroundColor = generateRandomColor()
-      darkenColor(item)
+      darkenColor(gridItem)
     })
     
   })
@@ -128,4 +125,22 @@ btnGumichka.addEventListener('click', ()=> {
   })
 
 
-createDivs(); //
+  //*sizes
+
+btnBigSize.addEventListener('click', ()=> {
+  createDivs(100)
+})
+
+
+btnMediumSize.addEventListener('click', ()=> {
+  createDivs(50)
+})
+
+
+btnSmallSize.addEventListener('click', ()=> {
+  createDivs(20)
+})
+
+//*sizes
+
+createDivs(); 
